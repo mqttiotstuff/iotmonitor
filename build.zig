5e2b190df50a0dfbca45318d05b0190d033e58bd
Patrice Freydiere <frett27@frett27.net>
2020-04-24 22:57:06 +0200

    clean up

--- build.zig
+++ build.zig
@@ -9,7 +9,7 @@ const Target = std.Target;
 pub fn build(b: *Builder) void {
     const exe = b.addExecutable("iotmonitor", "iotmonitor.zig");
     exe.setBuildMode(builtin.Mode.Debug);
-    // exe.setBuildMode(builtin.Mode.ReleaseFast);
+    // exe.setBuildMode(builtin.Mode.ReleaseSafe);
 
     exe.addPackagePath("toml", "zig-toml/src/toml.zig");
 
@@ -23,16 +23,10 @@ pub fn build(b: *Builder) void {
 
     exe.addLibPath("paho.mqtt.c/build/output");
     exe.addLibPath("paho.mqtt.c/src");
-    // exe.linkSystemLibrary("paho-mqtt3c");
     exe.addObjectFile("paho.mqtt.c/src/libpaho-mqtt3c.a");
 
     // these libs are needed by leveldb backend
     exe.linkSystemLibrary("leveldb");
-    // exe.addObjectFile("leveldb/libleveldb.a");
-    // exe.linkSystemLibrary("c++");
-    //exe.linkSystemLibrary("c++abi");
-
-    // exe.setBuildMode(b.standardReleaseOptions());
 
     b.default_step.dependOn(&exe.step);
 }

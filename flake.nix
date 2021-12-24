329ce7ce47b0ed0cf09f79442136923bb1a8e688
Patrice Freydiere <frett27@gmail.com>
2021-12-24 20:38:47 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Flake for building Iotmonitor";
+  # inputs = [ zig git cmake leveldb pandoc ];
+  inputs = { 
+      
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+      
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    defaultPackage.x86_64-linux =
+      # Notice the reference to nixpkgs here.
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "iotmonitor";
+        src = self
+        buildInputs = [ zig git cmake leveldb pandoc ];
+        configurePhase = ''
+          ls
+          zig version
+        '';
+
+        buildPhase = ''
+          make
+          zig build
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          mv bin/iotmonitor $out/bin
+        '';
+
+      };
+
+  };
+}

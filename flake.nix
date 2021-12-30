4c665d4607d8b672ac0e9b00bfa23904bf58c20d
Patrice Freydiere <frett27@gmail.com>
2021-12-31 00:27:48 +0100

    update flake.nix & locks

--- flake.nix
+++ flake.nix
@@ -2,7 +2,12 @@
   description = "Flake for building Iotmonitor";
   # inputs = [ zig git cmake leveldb pandoc ];
   inputs = { 
-      nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+
+      #  nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+      # To have zig 0.9, unstable one, .. 
+nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/d9c13cf44ec1b6de95cb1ba83c296611d19a71ae.tar.gz";
+
+
   };
 
   outputs = { self, nixpkgs }: {

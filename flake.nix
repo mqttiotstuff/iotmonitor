6a07e12cde77aafd69a0bbc2d5cfdb3058d29d1b
Patrice Freydiere <frett27@gmail.com>
2022-12-11 22:52:12 +0100

    indent

--- flake.nix
+++ flake.nix
@@ -1,42 +1,42 @@
 {
   description = "Flake for building Iotmonitor";
   # inputs = [ zig git cmake leveldb pandoc ];
-  inputs = { 
-      nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/22.05.tar.gz";
-      flake-utils.url = "github:numtide/flake-utils";
-      zig.url = "github:mitchellh/zig-overlay";
+  inputs = {
+    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/22.05.tar.gz";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig.url = "github:mitchellh/zig-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, zig }: 
+  outputs = { self,  zig, nixpkgs, flake-utils, }:
 
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
-	packages.iotmonitor = 
-        # Notice the reference to nixpkgs here.
-        pkgs.stdenv.mkDerivation {
-        name = "iotmonitor";
-        src = self; 
-        buildInputs = [ pkgs.zig pkgs.git pkgs.cmake pkgs.leveldb pkgs.pandoc ];
-        configurePhase = ''
-          ls
-          zig version
-        '';
+        packages.iotmonitor =
+          # Notice the reference to nixpkgs here.
+          pkgs.stdenv.mkDerivation {
+            name = "iotmonitor";
+            src = self;
+            buildInputs =
+              [ pkgs.zig pkgs.git pkgs.cmake pkgs.leveldb pkgs.pandoc ];
+            configurePhase = ''
+              ls
+              zig version
+            '';
 
-        buildPhase = ''
-          make
-          zig build -fstage1
-        '';
+            buildPhase = ''
+              make
+              zig build -fstage1
+            '';
 
-        installPhase = ''
-          mkdir -p $out/bin
-          mv bin/iotmonitor $out/bin
-        '';
+            installPhase = ''
+              mkdir -p $out/bin
+              mv bin/iotmonitor $out/bin
+            '';
 
-      };
+          };
 
-      defaultPackage = self.packages.${system}.iotmonitor;
-      }
-    );
- 
+        defaultPackage = self.packages.${system}.iotmonitor;
+      });
+
 }
